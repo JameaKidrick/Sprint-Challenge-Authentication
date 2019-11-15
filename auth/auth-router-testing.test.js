28e2ecbf25d511d6adb62d148ce48ad25f4e3d26
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-11-15 13:19:27 -0500

    finished testing

--- auth/auth-router-testing.test.js
+++ auth/auth-router-testing.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const server = require('../api/server');
+const db = require('../database/dbConfig');
 
 // CHECKING TO MAKE SURE THE ENVIRONMENT IS IN TESTING MODE, NOT DEVELOPMENT
 test("should set db environment to testing", function() {
@@ -8,20 +9,82 @@ test("should set db environment to testing", function() {
 
 describe('auth-router testing', () => {
   describe('POST register', () => {
+    beforeEach(async () => {
+      await db('users').truncate();
+    });
+    test(`RETURNS MESSAGE WITH USER'S INFO`, () => {
+      return request(server)
+      .post('/api/auth/register')
+      .send({
+        username: 'username',
+        password: 'password'
+      })
+      .then(response => {
+        expect(response.status).toEqual(201)
+        console.log('REGISTER MESSAGE', response.body.user)
+        expect(response.body.user.username).toBe('username')
+      })
+    })
 
+    test('receive 400 for not having username', () => {
+      return request(server)
+      .post('/api/auth/register')
+      .send({
+        username: '',
+        password: 'password'
+      })
+      .then(response => {
+        expect(response.status).toEqual(400)
+      })
+    })
   })
 
-  describe('GET login', () => {
-    
+  describe('POST login', () => {
+    test('return error message when wrong username and password are given', () => {
+      return request(server)
+      .post('/api/auth/login')
+      .send({
+        username: 'wrongusername',
+        password: 'wrongpassword'
+      })
+      .then(response => {
+        expect(response.status).toEqual(401)
+        console.log('LOGIN ERROR MESSAGE', response.body)
+        expect(response.body).toEqual({ message: 'Invalid credentials. Try again later.' })
+      })
+    })
+
+    test('receive 400 for not having username', () => {
+      return request(server)
+      .post('/api/auth/login')
+      .send({
+        username: '',
+        password: 'password'
+      })
+      .then(response => {
+        expect(response.status).toEqual(400)
+      })
+    })
   })
-})
+});
 
 describe('jokes-router testing', () => {
-  test('GET jokes', async () => {
-    const response = await request(server).get('/api/jokes')
-    .auth('username', 'password')
-    .set('Accept', 'application/json')
-    .expect(200);
-    expect(response.status).toEqual(200)
+  describe('GET jokes', () => {
+    test('receive 401 for not providing proper auth', () => {
+      return request(server)
+      .get('/api/jokes')
+      .set('Authorization', 'token')
+      .then((response) => {
+        expect(response.status).toEqual(401)
+      })
+    })
+
+    test('receive 400 for not providing any credentials', () => {
+      return request(server)
+      .get('/api/jokes')
+      .then((response) => {
+        expect(response.status).toEqual(400)
+      })
+    })
   })
 })
